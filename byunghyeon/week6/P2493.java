e98f4e917c545b08033087353831d8260c1a70b3
bangbang444 <kim1228com@naver.com>
2025-12-09 19:29:39 +0900

    P2493 문제 다시올림

--- byunghyeon/week6/P2493.java
+++ byunghyeon/week6/P2493.java
@@ -3,54 +3,45 @@ package byunghyeon.week6;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class P2493 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        Integer[] arr = new Integer[N];
+        int[] height = new int[N];
         for(int i = 0; i < N; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            height[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 1. 모두 양수인 경우
-        if(arr[0] >= 0 && arr[arr.length-1] >= 0){
-            System.out.println(arr[0] + " " + arr[1]);
-            return;
-        }
-        // 2. 모두 음수인 경우
-        if(arr[0] <= 0 && arr[arr.length-1] <= 0){
-            System.out.println(arr[arr.length-2] + " " + arr[arr.length-1]);
-            return;
-        }
+        int[] answer = new int[N];
 
-        // 3. 음수 양수 섞인 경우
-        int min = Integer.MAX_VALUE;
-        int[] ans = new int[2];
-        int start = 0, end = arr.length-1;
-        while(start < end){
-            int n1 = arr[start];
-            int n2 = arr[end];
+        Stack<int []> stack = new Stack<>();
+        for(int i = 0; i < N; i++){
+            int cur = height[i];
 
-            int diff = Math.abs(n1+n2);
-            if(diff < min){
-                //System.out.println(n1 + " " + n2 + " " + min);
-                min = diff;
-                ans = new int[]{n1, n2};
+            while(!stack.empty() && cur > stack.peek()[0]){
+                stack.pop();
             }
 
-            if(Math.abs(arr[start+1] + arr[end]) > Math.abs(arr[start] + arr[end-1])){
-                end--;
-            }else{
-                start++;
+
+            if(stack.empty()){
+                answer[i] = 0;
             }
+            else answer[i] = stack.peek()[1]+1;
+
+            stack.push(new int[]{cur, i});
         }
 
-        System.out.println(ans[0] + " " + ans[1]);
+
+        StringBuilder ans = new StringBuilder();
+        for (int i : answer) {
+            ans.append(i).append(" ");
+        }
+        ans.append("\n");
+        System.out.print(ans);
     }
 }
