ccafc4fde5d75e793ae45dda1d76b937f674a856
이동욱 <du123kim@naver.com>
2026-01-07 13:48:01 +0900

    11주차(1월7일)

--- donguk/week11/BOJ_22866.java
+++ donguk/week11/BOJ_22866.java
@@ -4,12 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class BOJ_22866 {
-    static Stack<Integer> Lstack;
-    static Stack<Integer> Rstack;
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(bf.readLine());
@@ -17,6 +16,7 @@ public class BOJ_22866 {
 
         int[] numbers = new int[N];
         ArrayList<Integer>[] indexes = new ArrayList[N];
+        int[] cnt = new int[N];
 
         for (int i = 0; i < N; i++) {
             indexes[i] = new ArrayList<>();
@@ -25,13 +25,45 @@ public class BOJ_22866 {
         }
 
         Stack<Integer> stack = new Stack<>();
+        Arrays.fill(cnt, 0);
+
         //왼쪽->오른쪽
         for (int i = 0; i < N; i++) {
-            while(!stack.isEmpty())
+            while (!stack.isEmpty() && numbers[stack.peek()] <= numbers[i]) {
+                stack.pop();
+            }
+
+            cnt[i] += stack.size();
+            if(!stack.isEmpty()) indexes[i].add(stack.peek());
+            stack.push(i);
         }
 
+        stack.clear();
+
         //오른쪽 -> 왼쪽
+        for (int i = N-1; i >=0; i--) {
+            while (!stack.isEmpty() && numbers[stack.peek()]<= numbers[i]) {
+                stack.pop();
+            }
 
+            cnt[i] += stack.size();
+            if(!stack.isEmpty()) indexes[i].add(stack.peek());
+            stack.push(i);
+        }
 
+        //가장 가까운 인덱스 조사 및 출력
+        for (int i = 0; i < N; i++) {
+            if (cnt[i] == 0) {
+                System.out.println(0);
+            } else {
+                int tmp = indexes[i].get(0);
+                for (int idx : indexes[i]) {
+                    if(Math.abs(idx-i) < Math.abs(tmp-i))
+                        tmp = idx;
+                }
+
+                System.out.println(cnt[i]+" "+(tmp+1));
+            }
+        }
     }
 }
